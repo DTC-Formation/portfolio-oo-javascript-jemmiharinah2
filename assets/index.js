44f02f499d260bbd99e978b57a1fbcecbdb89042
jemmiharinah2 <jemima1rakotonarivo@gmail.com>
2023-07-25 18:04:50 +0200

    portfolio

---
+++ assets/index.js
@@ -0,0 +1,58 @@
+
+let jemima = {
+};
+
+
+let aboutMeSection = document.getElementById("about-me");
+aboutMeSection.innerHTML = `
+<img src="./assets/img/ko_n.jpg"  class="image"  height="300px" alt="Jemima Rakotonarivo">
+    
+    <h1>A PROPOS DE MOI</h1>
+    <h2>Bonjour, je suis Jemima  <span>RAKOTONARIVO </span> </h2> 
+    <p> Passionnée par le marketing digital et les métiers du Web. Le marketing digital et les métiers du web sont bien plus qu'une simple carrière pour moi. C'est une passion qui m'anime et me pousse à toujours repousser mes limites pour atteindre l'excellence dans ce domaine en constante évolution.</p>
+`;
+
+
+let skillsSection = document.getElementById("skills");
+skillsSection.innerHTML = `
+
+    <h1>Mes compétences  <img src="./assets/img/comp.png" height="70px" alt=""</h1>
+
+
+        <h3>HTML:</h3> <p>niveau débutant</p>
+        <h3>CSS:</h3> <p>niveau débutant</p>
+        <h3>Canva:</h3> <p>niveau intermediaire</p>
+`;
+
+
+let experiencesSection = document.getElementById("experiences");
+experiencesSection.innerHTML = `
+    <h1>Mes expériences  <img src="./assets/img/eee.png" height="70px"</h1>
+    <h3>CHARGEE DE RECRUTEMENT VIRTUEL | Eufonie/Adensourcing HTML:</h3>   <p>Antananarivo  <br>Déc 2021 – Avril 2022</p><br>  
+    <h3>RESPONSABLE COMMERCIALE ET RESSOURCES HUMAINES | LYAS et NOVA Design e-commerce</h3>   <p>BESARETY<br>Avril - Juillet 2021</p><br>  
+    <h3>COMMERCIALE ET DESIGNER ART HAND CRAFT | VIRGO Hand Craft </h3>  <p>NAIROBI, KENYA<br>Nov- 2019- Janvier 2021</p><br>  
+`;
+
+let educationSection = document.getElementById("education");
+educationSection.innerHTML = `
+    <h1>Mes formations  <img src="./assets/img/cap.png" height="70px"</h1>
+    
+         <h3>Licence en Marketing et Commerce</h3>  <p>IUM Institut Universitaire de Madagascar 2018/2019<p><br> 
+         <h3>DTS en Commerce International et Opérations douanières</h3>  <p>ISECOM - Antananarivo 2015 / 2016</p><br> 
+         <h3>BACCALAUREAT</h3>  <p>Lycée Ranohavimanana - Ambatondrazaka 2014 / 2015</p><br> 
+
+`;
+
+
+let additionalEducationSection = document.getElementById("additional-education");
+additionalEducationSection.innerHTML = `
+    <h1>Mes formations additionnelles  <img src="./assets/img/book.png" height="70px"</h1>
+    
+        <h3>FORMATION en Développement Web</h3>   <P> DTC-Digital Training Center 2023</p><br>
+        <h3>FORMATION Talent Acquisition </h3> <P> Talenta Sourcing/ Skillz Antananarivo 2021</p><br>
+        <h3>FORMATION NEW LEADER FOR AFRICA</h3> <P> Together for A New Africa Nairobi KENYA 2020</p><br>
+        <h3>DELF DIPLOME DE LANGUE FRANÇAISE</h3>  <P>  Alliance Française - Ambatondrazaka 2010</p><br>
+    
+`;
+
+
